39baa798edcc7459debade15889e8d6406a5bece
Stefanie-A <102748373+Stefanie-A@users.noreply.github.com>
2023-01-16 22:29:31 +0100

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person: ")
+madLib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madLib)
